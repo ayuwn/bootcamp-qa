cb5a37c234a28d2c54d12aeb9e1f5050553600e7
ayuwn <ayuwidianingsih@students.unnes.ac.id>
2026-06-09 21:13:21 +0700

    Update

--- session4/function.js
+++
@@ -1,9 +0,0 @@
-let panjang1 = 5;
-let panjang2 = 10;
-
-function hitungLuasPersegiPanjang(panjang, lebar) {
-    return panjang * lebar;
-}
-
-let luas1 = hitungLuasPersegiPanjang(panjang1, panjang2);
-console.log("Luas Persegi Panjang: " + luas1);
